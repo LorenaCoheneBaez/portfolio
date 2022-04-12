4669d98bad25508f3b19f72f42f69927a8b20062
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-04-12 20:40:15 -0300

    Fix js to deploy

--- public/js/videoTheme.js
+++ public/js/videoTheme.js
@@ -1,45 +1,56 @@
 window.addEventListener("load", function () {
     //Modo claro/oscuro
     const themeBtn = document.getElementById('theme-btn');
-    themeBtn.addEventListener('click', () => {
-        let iconColor = document.getElementById("color-icon-header")
+    if (themeBtn){
+        themeBtn.addEventListener('click', () => {
+            let iconColor = document.getElementById("color-icon-header")
 
-        iconColor.classList.toggle('bi-moon-fill');
+            iconColor.classList.toggle('bi-moon-fill');
 
-        let e = document.body;
-        e.classList.toggle('light-mode')
-    })
+            let e = document.body;
+            e.classList.toggle('light-mode')
+        }, false)
+    }
+    
     
     //Controles video
     const demo = document.getElementById('/#leaf3');
 
     const btnClose = document.getElementById('close-video-leaf')
+    if(demo){
+        demo.addEventListener('click', (e) => {
 
-    demo.addEventListener('click', ()=>{
+            let element = document.getElementById('video-leaf');
+            element.style.display = "block"
+        }, false)        
+    }
 
-        let element = document.getElementById('video-leaf');
-        element.style.display = "block"
-    })
-
-    btnClose.addEventListener('click', () =>{
-        let element = document.getElementById('video-leaf');
-        element.style.display = "none"
-    })
+    if(btnClose){
+        btnClose.addEventListener('click', () => {
+            let element = document.getElementById('video-leaf');
+            element.style.display = "none"
+        }, false)
+    }
+    
 
     const demoGarden = document.getElementById('/#garden4');
 
     const btnCloseGarden = document.getElementById('close-video')
+    if(demoGarden){
+        demoGarden.addEventListener('click', () => {
 
-    demoGarden.addEventListener('click', () => {
+            let element = document.getElementById('video-garden');
+            element.style.display = "block"
 
-        let element = document.getElementById('video-garden');
-        element.style.display = "block"
-
-    })
-
-    btnCloseGarden.addEventListener('click', () => {
-        let element = document.getElementById('video-garden');
-        element.style.display = "none"
-    })
+        }, false)
+    }
+    
+    if(btnCloseGarden){
+        btnCloseGarden.addEventListener('click', () => {
+            let element = document.getElementById('video-garden');
+            element.style.display = "none"
+        }, false)
+    }
+   
 }
 )
